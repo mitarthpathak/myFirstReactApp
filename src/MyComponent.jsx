25dd86498d03205a961399f803cc9691b38a39fc
mitarth pathak <97668137+mitarthpathak@users.noreply.github.com>
2026-09-26 20:01:07 +0530

    practiced today

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,9 +3,11 @@ import React, {use, useState} from "react";
 function MyComponent(){
 
     //array destructering
-    const [name, setName] = useState("Guest");
+    const [name, setName] = useState("");
     const [age, setAge] = useState(0);
     const [isEmployed, setIsEmployed] = useState(true);
+    const [inc, addCounter] = useState(0);
+    
 
     const updateName = () => {
         setName("Mitarth");
@@ -19,16 +21,38 @@ function MyComponent(){
         setIsEmployed(!isEmployed);
     }
 
+    const addCounterButton = () => {
+        addCounter(inc + 1);
+    }
+    const minCounterButton = () => {
+        addCounter(inc - 1);
+    }
+    const setZeroButton = () => {
+        addCounter(0);
+    }
+
+    function handleNameChange(event){
+        setName(event.target.value);
+    }
+
     
 
     return(
         <div>
             <p>Name : {name}</p>
-            <button onClick={updateName}>Set Name</button>
+            <input value={name} onChange={handleNameChange}></input>
             <p>Age : {age}</p>
             <button onClick={incrementAge}>Set Age</button>
-            <p>IsEmployed {isEmployed ? "yes" : "No"}</p>
+            <p>IsEmployed:  {isEmployed ? "yes" : "No"}</p>
             <button onClick={toggleEmploymentStatus}>Change the Emoployment</button>
+            <h1><cent>COUNTER</cent></h1>
+            <p>counter : {inc}</p>
+            <button onClick={addCounterButton}>+</button>
+            <button onClick={minCounterButton}>-</button>
+            <button onClick={setZeroButton}> Reset</button>
+
+
+
         </div>
     )
 }
